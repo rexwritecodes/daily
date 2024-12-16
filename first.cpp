8cac6503e7f3524c4f20f97c19e6692b05b245e1
rexwritecodes <pranavpanchwatkar@outloo.com>
2024-12-16 18:45:40 +0530

    Add missing files

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    char n[50];
+    scanf("%s", n);
+    printf("Hello %s",n);
+    scanf("%s", n);
+    printf("Hello %s",n);
+    scanf("%s", n);
+    printf("Hello %s",n);
+    
+}
